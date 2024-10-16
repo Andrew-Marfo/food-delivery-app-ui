c096d3028be8aaa30d590acb8a890fb49ab70ab9
Andrew Marfo <andrewmarfo23@gmail.com>
2024-10-16 18:15:24 +0000

    category tag

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:food_delivery_ui/pages/homepage/widgets/custom_appbar.dart';
-import 'package:food_delivery_ui/pages/homepage/widgets/promotion_card.dart';
+import 'package:food_delivery_ui/pages/home/widgets/categories_tags.dart';
+import 'package:food_delivery_ui/pages/home/widgets/custom_appbar.dart';
+import 'package:food_delivery_ui/pages/home/widgets/promotion_card.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -14,6 +15,7 @@ class Home extends StatelessWidget {
       body: const Column(
         children: [
           PromotionCard(),
+          CategoriesTags(),
         ],
       ),
     );
---
+++ lib/pages/home/widgets/categories_tags.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:iconly/iconly.dart';
+
+class CategoriesTags extends StatelessWidget {
+  const CategoriesTags({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final categoryList = [
+      {'name': 'All', 'icon': 'IconlyLight.category'},
+      {'name': 'Pizza', 'icon': 'IconlyLight.category'},
+      {'name': 'Sushi', 'icon': 'IconlyLight.category'},
+    ];
+    return Container(
+      color: Colors.amber,
+      height: 50,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categoryList.length,
+        itemBuilder: (context, index) {
+          return Container(
+            margin: const EdgeInsets.only(right: 10),
+            child: Stack(
+              children: [
+                Chip(
+                  shape: const StadiumBorder(
+                    side: BorderSide(
+                      width: 2,
+                      color: Color(0xFFF27D44),
+                    ),
+                  ),
+                  label: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 40),
+                        child: Text(
+                          categoryList[index]['name']!,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 2,
+                  // bottom: 2,
+                  left: 0,
+                  width: 45,
+                  height: 45,
+                  child: Container(
+                    height: 40,
+                    decoration: const BoxDecoration(
+                      color: Color(0xFFF27D44),
+                      shape: BoxShape.circle,
+                    ),
+                    child: const Icon(
+                      IconlyLight.category,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
